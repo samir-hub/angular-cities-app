ae91b59472f12674dad4d913850a85c5c65ec3b6
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-09 17:21:58 -0400

    added angular material

--- cities-app/src/app/app.module.ts
+++ cities-app/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
-import { qolService } from './services/qol.service'; 
+import { qolService } from './services/qol.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
@@ -11,7 +14,10 @@ import { qolService } from './services/qol.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [qolService],
   bootstrap: [AppComponent]
