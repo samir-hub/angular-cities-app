7918b3cb2e0b863432500da7c73002d86630394d
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-11 15:20:03 -0400

    setting up single city routing

--- cities-app/src/app/app-routing.module.ts
+++ cities-app/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CitiesListComponent } from './cities/cities-list/cities-list.component'; 
 import { HomeComponent } from './home/home.component'; 
+import { CityComponent } from './cities/city/city.component'; 
 
 const routes: Routes = [
-  {path: 'cities', component: CitiesListComponent},
   {path: '', component: HomeComponent},
+  {path: 'cities', component: CitiesListComponent},
+  {path: 'city/:id', component: CityComponent}
+  
 ];
 
 @NgModule({
