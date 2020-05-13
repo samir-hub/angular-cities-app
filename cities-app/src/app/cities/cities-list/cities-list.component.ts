002aedf69aecaf25c388ae2c0164cb4384248b3c
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-13 13:42:35 -0400

    city list automatically updates after submitting form

--- cities-app/src/app/cities/cities-list/cities-list.component.ts
+++ cities-app/src/app/cities/cities-list/cities-list.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Cities } from '../../classes/cities';
-import { cityService } from '../../services/city.service'; 
+import { Component, OnInit } from "@angular/core";
+import { Cities } from "../../classes/cities";
+import { cityService } from "../../services/city.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-cities-list',
-  templateUrl: './cities-list.component.html',
-  styleUrls: ['./cities-list.component.css']
+  selector: "app-cities-list",
+  templateUrl: "./cities-list.component.html",
+  styleUrls: ["./cities-list.component.css"],
 })
 export class CitiesListComponent implements OnInit {
-
-  constructor(private _cityService: cityService) { }
+  constructor(
+    private _cityService: cityService,
+    private formBuilder: FormBuilder
+  ) {this.cityForm = this.formBuilder.group({
+    id: "",
+    name: "",
+  });}
 
   listCities: Cities[];
+  cityForm;
 
   ngOnInit() {
     this._cityService.getCities().subscribe((data) => {
@@ -19,4 +26,12 @@ export class CitiesListComponent implements OnInit {
     });
   }
 
+  onSubmit(cityData) {
+    // Process checkout data here
+    this._cityService.addCity(cityData).subscribe(
+      data => {
+        this.listCities.push(data); 
+      }
+    )
+  }
 }
